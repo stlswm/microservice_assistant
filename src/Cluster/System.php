52b8081e10a891eba09c7b87f4bdfc14ad6366a1
stlswm <691207572@qq.com>
2021-05-17 13:51:08 +0800

    system add inner direct req

--- src/Cluster/System.php
+++ src/Cluster/System.php
@@ -79,7 +79,7 @@ class System
      * @param  string  $ip
      * @param  string  $authStr
      * @param  string  $random
-     * @param  int  $timestamp
+     * @param  int     $timestamp
      * @return bool
      */
     public static function isInnerReq(string $ip, string $authStr, string $random, int $timestamp): bool
@@ -118,7 +118,7 @@ class System
      * 子系统内部请求
      * @param  string  $systemAlias  系统别名|完整的域名地址
      * @param  string  $router
-     * @param        $data
+     * @param          $data
      * @return array
      * @throws Exception
      */
@@ -166,6 +166,58 @@ class System
                 return (array)$resData;
             }
             return IO::fail(ErrCode::NetErr, "无法解析API：{$domain}{$router} 返回：".$response);
+        } catch (Exception $e) {
+            return IO::fail(ErrCode::NetErr, $e->getMessage());
+        }
+    }
+
+    /**
+     * 子系统内部请求直接发送
+     * @param  string  $url  系统地址
+     * @param          $data
+     * @return array
+     * @throws Exception
+     */
+    public static function innerDirectRequest(string $url, $data): array
+    {
+        if (empty(self::$clusterKey)) {
+            throw new Exception('必需设置集群秘钥');
+        }
+        try {
+            if (is_array($data)) {
+                $data = json_encode($data);
+            }
+            $random = self::getRandomString(32);
+            $timestamp = time();
+            //请求对象
+            $headers = [
+                "Cluster-Random: ".$random,
+                "Cluster-Timestamp: ".$timestamp,
+                "Cluster-Auth: ".self::generatorAuthKey($random.'&'.$timestamp),
+                "Content-Type: application/json",
+            ];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 8000);
+            curl_setopt($ch, CURLOPT_TIMEOUT_MS, 30000);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            $response = curl_exec($ch);
+            $resInfo = curl_getinfo($ch);
+            curl_close($ch);
+            if ($resInfo["http_code"] != 200) {
+                throw new Exception("response status code is not valid. status code: ".$resInfo["http_code"]);
+            }
+            $resData = json_decode($response, true);
+            if (is_array($resData)) {
+                return (array)$resData;
+            }
+            return IO::fail(ErrCode::NetErr, "无法解析API：{$url} 返回：".$response);
         } catch (Exception $e) {
             return IO::fail(ErrCode::NetErr, $e->getMessage());
         }
